01137af6cb671cb3bed4b2c5a1b4067dc3d65333
simonol1 <simondanolearyis@gmail.com>
2017-05-05 11:30:37 +1200

    added engine info for handlebars

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ var app =express()
 
 app.use(express.static('./public'))
 
+app.use('/',routes)
+app.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+
+[11:24]
+app.set('view engine', 'hbs')
+
 app.use('/', routes)
 
 module.exports = app
