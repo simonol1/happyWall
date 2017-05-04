387eaeaa60079cf03aa35b05f4ed7a580f51fb30
Zac Metin <zac.metin@gmail.com>
2017-05-05 11:39:46 +1200

    add router

--- routes.js
+++ routes.js
@@ -1,8 +1,18 @@
 var express = require('express')
+var posts= require ('./data.json')
 var router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('Happy Wall')
 })
+
+//post
+router.get('/', (req, res) => {
+  res.render ('')
+})
+
+//form
+
+
 
 module.exports = router
